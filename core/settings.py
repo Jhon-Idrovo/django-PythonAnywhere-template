4d1b4cc072c77783b44ba7589b87ed311847c1f9
Jhon-Idrovo <77032831+Jhon-Idrovo@users.noreply.github.com>
2021-05-25 15:11:46 -0500

    added mysqlclient to requirements

--- core/settings.py
+++ core/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '8*y5azy1kzu+8vg8gs_75zeflgse^gobl9ezioy_$6824q)1&*'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
